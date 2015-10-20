f5f23f48e6dbf924637d09c1915fe3d2d59c2168
mitch <mitch@repositpower.com>
2015-10-20 20:40:27 +1100

    inital structure

---
+++ README.rst
@@ -0,0 +1,7 @@
+===============
+TimeSync Solver
+===============
+
+About
+=====
+Coming soon, when I figured it out.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    name='timesyncsolver',
+    version='0.0.1',
+    packages=['timesyncsolver', 'tests'],
+    url='https://github.com/mitchoneill/timesync-solver',
+    license='APACHE',
+    author='Mitch ONeill',
+    author_email='mitchoneill@gmail.com',
+    description='numerical solver for timesync',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Education',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
